da7aabd40ec7245da2cfc32bae63717ed2c3f2a2
kevin phan <kevin.phu@student.uts.edu.au>
2021-11-04 19:46:43 +1100

    pacstudnet title and subtitle,sprites,border, level 1 button scirps

--- Assets/Scripts/Level1_Load.cs
+++ Assets/Scripts/Level1_Load.cs
@@ -5,17 +5,26 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class Level1_Load : MonoBehaviour
 {
+    //public RectTransform loadingScreen;
     // Start is called before the first frame update
     void Start()
     {
-      
+        //loadingScreen.sizeDelta = new Vector2(Screen.width, Screen.height);
     }
 
     // Update is called once per frame
     void Update()
     {
-        SceneManager.LoadScene("1");
+       // SceneManager.LoadScene("1");
     }
 
- 
+    public void LoadFirstLevel()
+    {
+        DontDestroyOnLoad(this);
+        SceneManager.LoadSceneAsync(1);
+    }
+   /* public void StartButtonPressed()
+    {
+        Invoke("LoadFirstLevel", 1.0f);
+    }*/
 }
